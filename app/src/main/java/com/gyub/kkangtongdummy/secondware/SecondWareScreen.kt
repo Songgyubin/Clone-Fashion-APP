1e8e4dd40a062e2726d7ee3ffbe27eec828bcdf3
SongGyul <gyul@hellomarket.com>
2023-11-28 23:03:20 +0900

    [세컨웨어] 홈 화면 코드정리

---
+++ app/src/main/java/com/gyub/kkangtongdummy/secondware/SecondWareScreen.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright ⓒ 2011 HelloMarket Inc. All Rights Reserved.
+ */
+package com.gyub.kkangtongdummy.secondware
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * 세컨웨어 앱
+ *
+ * @author   Gyul
+ * @created  2023/11/28
+ */
+class SecondWareScreen {
+
+    @Composable
+    fun Curation() {
+        Text(text = "SecondWare")
+    }
+}
