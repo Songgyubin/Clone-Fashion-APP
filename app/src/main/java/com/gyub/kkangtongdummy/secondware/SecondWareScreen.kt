ec32fa8667e37c50c96cbee2d71729fae228d70c
gyu_b <thd0427__@naver.com>
2023-12-03 17:01:20 +0900

    [세컨웨어] 세컨웨어 앱 연결 (#5)
    * [세컨웨어] SecondWare 임시 테마 세팅
    
    * [세컨웨어] 홈 화면 코드정리
    
    * [세컨웨어] 세컨웨어 화면 연결
    
    ---------
    
    Co-authored-by: SongGyul <gyul@hellomarket.com>

---
+++ app/src/main/java/com/gyub/kkangtongdummy/secondware/SecondWareScreen.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright ⓒ 2011 HelloMarket Inc. All Rights Reserved.
+ */
+package com.gyub.kkangtongdummy.secondware
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+/**
+ * 세컨웨어 앱
+ *
+ * @author   Gyul
+ * @created  2023/11/28
+ */
+
+@Composable
+fun SecondWareScreen() {
+    Text(text = "SecondWare")
+}
