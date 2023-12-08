0dd4225765d6727e32dd4b26619b4b569178cba0
SongGyul <gyul@hellomarket.com>
2023-12-08 17:50:06 +0900

    [세컨웨어] 큐레이션 아이템 - 상품 이미지

--- app/src/main/java/com/gyub/kkangtongdummy/secondware/SecondWareScreen.kt
+++ app/src/main/java/com/gyub/kkangtongdummy/secondware/SecondWareScreen.kt
@@ -2,22 +2,31 @@ package com.gyub.kkangtongdummy.secondware
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gyub.kkangtongdummy.R
 import com.gyub.kkangtongdummy.ui.theme.SdsBlue05
 import com.gyub.kkangtongdummy.ui.theme.SecondWareTheme
@@ -43,7 +52,41 @@ fun SecondWareMain() {
 
 @Composable
 fun CurationItemList() {
+//    LazyHorizontalGrid(rows = 2, content =) {
+//
+//    }
+}
 
+@Preview
+@Composable
+fun CurationItemPreview() {
+    CurationItemImage()
+}
+
+@Composable
+fun CurationItemImage() {
+    Card {
+        Box {
+            val itemImageModifier = Modifier
+                .width(174.dp)
+                .height(154.dp)
+
+            Image(
+                modifier = itemImageModifier,
+                contentDescription = "curationItemImage",
+                painter = painterResource(id = R.drawable.sds_img_sample_item),
+                contentScale = ContentScale.Crop
+            )
+            val wishIconModifier = Modifier.align(Alignment.BottomEnd)
+
+            Image(
+                modifier = wishIconModifier
+                    .padding(10.dp),
+                painter = painterResource(id = R.drawable.sds_ico_heart_wish_normal_18),
+                contentDescription = "wishIcon"
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -67,7 +110,6 @@ fun CurationTitle() {
             Text(text = stringResource(R.string.prefix_curation_title))
 
             Text(text = getStyledText())
-
         }
 
         Text(
