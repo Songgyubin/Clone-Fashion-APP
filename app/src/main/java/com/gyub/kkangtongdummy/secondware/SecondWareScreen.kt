1fcac839c4bd24a69b5162ccfc46b3be1ac90667
gyu_b <thd0427__@naver.com>
2023-12-19 22:25:27 +0900

    [세컨웨어] 상품 큐레이션 UI 추가 (#6)
    * [세컨웨어] 큐레이션 타이틀 파트
    
    * [세컨웨어] 큐레이션 아이템 - 상품 이미지
    
    * [세컨웨어] 큐레이션 상품 카드
    
    * [세컨웨어] 상품 큐레이션 완료
    
    ---------
    
    Co-authored-by: SongGyul <gyul@hellomarket.com>

--- app/src/main/java/com/gyub/kkangtongdummy/secondware/SecondWareScreen.kt
+++ app/src/main/java/com/gyub/kkangtongdummy/secondware/SecondWareScreen.kt
@@ -1,10 +1,42 @@
-/*
- * Copyright ⓒ 2011 HelloMarket Inc. All Rights Reserved.
- */
 package com.gyub.kkangtongdummy.secondware
 
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gyub.kkangtongdummy.R
+import com.gyub.kkangtongdummy.ui.theme.SdsBlue05
+import com.gyub.kkangtongdummy.ui.theme.SecondWareTheme
 
 /**
  * 세컨웨어 앱
@@ -12,8 +44,141 @@ import androidx.compose.runtime.Composable
  * @author   Gyul
  * @created  2023/11/28
  */
-
 @Composable
 fun SecondWareScreen() {
-    Text(text = "SecondWare")
+    SecondWareTheme {
+        Surface {
+            CurationItemGroup()
+        }
+    }
 }
+
+@Composable
+fun SecondWareMain() {
+
+}
+
+@Preview
+@Composable
+fun CurationItemGroupPreview() {
+    CurationItemGroup()
+}
+
+@Composable
+fun CurationItemGroup() {
+    Column {
+        CurationTitle(
+            Modifier
+                .padding(start = 20.dp, bottom = 10.dp)
+        )
+        CurationItemList()
+    }
+}
+
+@Preview
+@Composable
+fun CurationItemListPreview() {
+    CurationItemList()
+}
+
+@Composable
+fun CurationItemList() {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(start = 20.dp, end = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(14.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(curationCardItemHeight * 2 + 14.dp)
+    ) {
+        items(20) {
+            CurationCardItem()
+        }
+    }
+}
+
+@Composable
+fun CurationCardItem() {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .width(132.dp)
+    ) {
+        Box {
+            val itemImageModifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(10.dp))
+                .height(132.dp)
+
+            Image(
+                modifier = itemImageModifier,
+                contentDescription = "curationItemImage",
+                painter = painterResource(id = R.drawable.sds_img_sample_item),
+                contentScale = ContentScale.Crop
+            )
+            val wishIconModifier = Modifier.align(Alignment.BottomEnd)
+
+            Image(
+                modifier = wishIconModifier
+                    .padding(10.dp),
+                painter = painterResource(id = R.drawable.sds_ico_heart_wish_normal_18),
+                contentDescription = "wishIcon"
+            )
+        }
+        Text(
+            text = "Nike", modifier = Modifier
+                .background(Color.White)
+                .padding(start = 4.dp)
+                .fillMaxWidth()
+        )
+        Text(
+            text = "15,000원", modifier = Modifier
+                .background(Color.White)
+                .padding(start = 4.dp)
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun CurationTitle(modifier: Modifier) {
+    val context = LocalContext.current
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { showToast(context, "링크 이동") },
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom,
+    ) {
+        Column {
+            Text(text = stringResource(R.string.prefix_curation_title))
+
+            Text(text = getStyledText())
+        }
+
+        Text(
+            text = stringResource(R.string.view_all)
+        )
+    }
+}
+
+@Composable
+fun getStyledText(): AnnotatedString {
+    return buildAnnotatedString {
+        withStyle(style = SpanStyle(color = SdsBlue05)) {
+            append(stringResource(R.string.national_geographic))
+        }
+        append(" ")
+        append(stringResource(R.string.are_you_looking_for))
+    }
+}
+
+private fun showToast(context: Context, message: String) {
+    Toast
+        .makeText(context, message, Toast.LENGTH_SHORT)
+        .show()
+}
+
+val curationCardItemHeight = 172.dp
---
+++ app/src/main/java/com/gyub/kkangtongdummy/ui/theme/SecondWareColor.kt
@@ -0,0 +1,37 @@
+package com.gyub.kkangtongdummy.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * SecondWare Color
+ *
+ * @author   Gyul
+ * @created  2023/12/05
+ */
+
+val SdsGray01 = Color(0xFFF1F3F7)
+val SdsGray02 = Color(0xFFE6E9EE)
+val SdsGray03 = Color(0xFFDCE0E5)
+val SdsGray04 = Color(0xFFCED3D9)
+val SdsGray05 = Color(0xFFC0C6CC)
+val SdsGray06 = Color(0xFFA1A8AF)
+val SdsGray07 = Color(0xFF868C95)
+val SdsGray08 = Color(0xFF697077)
+val SdsGray09 = Color(0xFF5B6167)
+val SdsGray10 = Color(0xFF4D5358)
+val SdsGray11 = Color(0xFF343A3F)
+val SdsGray12 = Color(0xFF21272A)
+val SdsGray13 = Color(0xFF121619)
+
+val SdsBlue01 = Color(0xFFEAEAFF)
+val SdsBlue02 = Color(0xFFD6D6FF)
+val SdsBlue03 = Color(0xFFBCBCFF)
+val SdsBlue04 = Color(0xFFA6A6FF)
+val SdsBlue05 = Color(0xFF8C8CFF)
+val SdsBlue06 = Color(0xFF7070FF)
+val SdsBlue07 = Color(0xFF5252FF) // Default Color
+val SdsBlue08 = Color(0xFF4040FF)
+
+
+
+
