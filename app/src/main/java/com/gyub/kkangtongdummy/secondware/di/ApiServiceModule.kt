c4b4992a87e601c6550586118addbd3c5e08973a
gyu_b <thd0427__@naver.com>
2023-12-21 00:39:58 +0900

    dagger, network 세팅 (#8)
    Co-authored-by: SongGyul

---
+++ app/src/main/java/com/gyub/kkangtongdummy/secondware/di/ApiServiceModule.kt
@@ -0,0 +1,15 @@
+package com.gyub.kkangtongdummy.secondware.di
+
+import dagger.Provides
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+object ApiServiceModule {
+//    @Singleton
+//    @Provides
+//    fun provideAuthService(retrofit: Retrofit.Builder): AuthService {
+//        return retrofit
+//            .build()
+//            .create(AuthService::class.java)
+//    }
+}
---
+++ app/src/main/java/com/gyub/kkangtongdummy/secondware/di/NetworkModule.kt
@@ -0,0 +1,60 @@
+package com.gyub.kkangtongdummy.secondware.di
+
+import android.util.Log
+import com.google.gson.Gson
+import com.gyub.kkangtongdummy.secondware.network.ApiUrl.BASE_URL
+import com.gyub.kkangtongdummy.secondware.network.NetworkUtil
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    @Named("RetrofitInterceptor")
+    fun provideRetrofitInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor { message ->
+            Log.d("SecondWare", "### Retrofit -- ${NetworkUtil.getPrettyLogs(message)}")
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        @Named("RetrofitInterceptor") interceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(NetworkUtil.createHeader())
+            .addInterceptor(interceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson() = Gson()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gson: Gson,
+    ): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+    }
+}
